af000932e8bf7277613c476d22b60d042a0ca34a
Livinus Felix Bassey <livinusba591@nemi.tskoli.is>
2020-02-07 11:15:23 +0000

    ok

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
     return render_template('index.html')
 
 
@@ -13,9 +12,6 @@ def home():
 @app.route('/a-hluti')
 def ahluti():
     return render_template('kennitala.html')
-=======
-    return render_template('default.html')
->>>>>>> dfb284844b7e8cbc3ff6cde38824ae81d791c742
 
 @app.route('/kt-sida/<kt>') 
 def ktsida(kt):
@@ -25,7 +21,6 @@ def ktsida(kt):
 
     return render_template('kt-sum.html', kt=kt, summa=summa)
 
-<<<<<<< HEAD
 #verkhluti B
 #database er utfærður sem tvívíður listi(dictionary)
 #id, fyrirsögn, frétt, e-mail
@@ -54,9 +49,6 @@ def news(id):
     return render_template('frett.html', frett=frettir[id], nr=id)
 
 #villuskilaborð
-=======
-
->>>>>>> dfb284844b7e8cbc3ff6cde38824ae81d791c742
 @app.errorhandler(404)
 def pagenotfound(error):
     return render_template('pagenotfound.html'), 404
